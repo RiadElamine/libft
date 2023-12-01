cbf67a3d3b573792963bc8378d58a2867f327c1b
Riad Elamine <relamine@e3r10p13.1337.ma>
2023-12-01 01:09:28 +0100

    finish

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: relamine <relamine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 17:47:34 by relamine          #+#    #+#             */
-/*   Updated: 2023/11/30 23:11:56 by relamine         ###   ########.fr       */
+/*   Updated: 2023/12/01 00:54:09 by relamine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,28 +26,28 @@ typedef struct s_list
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strrchr(const char *s, int c);
-char	*ft_strnstr(const char *str, const char *find, size_t len);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlcat(char *dest, const char *src, size_t size);
+size_t	ft_strlcat(char *dest, const char *src, size_t dstsize);
 char	*ft_strchr(const char *s, int c);
-void	*ft_memset(void *voidptr, int value, size_t count);
+void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memchr(const void *voids, int c, size_t size);
+void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_isprint(int c);
 int		ft_isdigit(int c);
 int		ft_isascii(int c);
 int		ft_isalpha(int c);
 int		ft_isalnum(int c);
-void	ft_bzero(void *ptr, size_t size);
+void	ft_bzero(void *s, size_t n);
 int		ft_atoi(const char *str);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
-char	*ft_substr(const char *s, unsigned int start, size_t len);
-char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putnbr_fd(int n, int fd);
